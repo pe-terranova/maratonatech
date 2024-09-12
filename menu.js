4ca37c14b7b2a8b173120771e4f1967f737c0572
pe-terranova <petrolandia.terranova@gmail.com>
2024-09-11 21:00:23 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function(){
+
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 250)
+
+
+
+})
+
+const botao = document.querySelector('#botao')
+
+botao.addEventListener('click', () => {
+    window.scroll({top: window.innerHeight, behavior: "smooth"})
+})
